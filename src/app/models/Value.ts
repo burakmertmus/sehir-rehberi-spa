7e8c1ab0cdd42b6b73239e2747452e3e79b2430d
Burak Mert Muş <burakmertmus@hotmail.com>
2021-06-29 02:26:32 +0300

    SPA Generated

---
+++ src/app/models/Value.ts
@@ -0,0 +1,24 @@
+export class Value {
+    
+    id? : number;
+    // public get id() : number {
+    //     return this._id;
+    // }
+    // public set id(v : number) {
+    //     this._id = v;
+    // }
+    
+    
+    name?: string;
+    // public get name() : string {
+    //     return this._name;
+    // }
+    // public set name(v : string) {
+    //     this._name = v;
+    // }
+    
+}
+// interface Value{
+//     id:number;
+//     name:string;
+// }
---
+++ src/app/value/value.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Value } from '../models/Value';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+
+  constructor(private http:HttpClient) { }
+  values:Value[]=[];
+
+  ngOnInit() {
+    this.getValues().subscribe(data=>{
+      this.values=data
+    });
+  }
+  getValues(){
+    return this.http.get<Value[]>("https://localhost:44300/values");
+  }
+
+}
