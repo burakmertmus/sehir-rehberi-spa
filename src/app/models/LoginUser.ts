a326f72955e7a0c59af97cde00910f1dc43f575f
Burak Mert Muş <burakmertmus@hotmail.com>
2021-07-03 14:43:38 +0300

    Login Started

---
+++ src/app/models/LoginUser.ts
@@ -0,0 +1,4 @@
+export interface LoginUser {
+    userName:string;
+    passworr:string;
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoginUser } from '../models/LoginUser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  path: string = "http://localhost:44300/auth"
 
+  login(loginUser: LoginUser) {
+    let headers = new HttpHeaders();
+    headers = headers.append("Context-Type", "application/json");
+    this.httpClient
+    .post(this.path + "login", loginUser, { headers: headers })
+    .subscribe(data => {
+      this.saveToken(data['tokenString']);
+    });
+  }
+  saveToken(token:string){
+    localStorage.setItem('token',token);
+  }
 }
