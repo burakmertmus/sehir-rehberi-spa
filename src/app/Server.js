abafd8487008f14bc4f5626b510d357aa7e2f8cf
Burak Mert Muş <burakmertmus@hotmail.com>
2021-07-06 01:02:30 +0300

    Fix

--- src/app/Server.js
+++ src/app/Server.js
@@ -4,11 +4,8 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/sehir-rehberi-spa'));
-
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/sehir-rehberi-spa/'}),
+    res.sendFile('app.component.html', {root: 'dist/sehir-rehberi-spa/'}),
 );
 
 // Start the app by listening on the default Heroku port
