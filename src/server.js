840f5582916ba64ce3568a26df4b23281fc8df61
Burak Mert Muş <burakmertmus@hotmail.com>
2021-07-06 01:21:30 +0300

    .

--- src/server.js
+++ src/server.js
@@ -4,9 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.get('/*', (req, res) =>
-    res.sendFile('app.component.html', {root: 'dist/sehir-rehberi-spa/'}),
-);
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
