fc2a56bd26fe72c634e4dce85a64723073dfe800
Burak Mert Muş <burakmertmus@hotmail.com>
2021-07-06 02:00:19 +0300

    .

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/sehir-rehberi-spa'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/sehir-rehberi-spa/'}),
